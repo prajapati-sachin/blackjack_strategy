293894ff2192f674008516736a1e9e9a0dea926b
prajapati-sachin <sachinkp1211@gmail.com>
2018-10-23 00:37:51 +0530

    correct the order

--- main.cpp
+++ main.cpp
@@ -17,8 +17,44 @@ void fill_reward(){
 	for(int i=0;i<18;i++){
 		int player_sum=i+4;
 		//with ACE
-		for(int j=21;j>=15;j--){
+		for(int j=20;j>=15;j--){
+			float ans=0;
+			int dealer_sum=j-4;
+			//for normal cards
+			for(int k=2;k<=9;k++){
+				if(dealer_sum+k>=17){
+					if(dealer_sum+k>player_sum){
+						ans+= p_noface*(-1);
+					}
+					else if(dealer_sum+k==player_sum){
+						ans+=0;
+					}
+					else{
+						ans+= p_noface*(1);
+					}
+				}
+				else if(dealer_sum+k<17){
+					ans+= p_noface*expected_reward[i][j+k];
+				}
+			}
 
+			//for face card
+				ans+= p_face*expected_reward[i][dealer_sum-2];
+			//for ACE
+			if(dealer_sum+1>=17){
+				if(dealer_sum+k>player_sum){
+						ans+= p_noface*(-1);
+				}
+				else if(dealer_sum+k==player_sum){
+					ans+=0;
+				}
+				else{
+					ans+= p_noface*(1);
+				}
+			}
+			else{
+				ans+= p_noface*expected_reward[i][j+1];
+			}
 		}
 		//without ACE
 		for(int j=14;j>=0;j--){
@@ -39,18 +75,7 @@ void fill_reward(){
 				}
 				else if(dealer_sum+k<17){
 					ans+= p_noface*expected_reward[i][j+k];
-				}
-				else{
-					if(dealer_sum+k>player_sum){
-						ans+= p_noface*(-1);
-					}
-					else if(dealer_sum+k==player_sum){
-						ans+=0;
-					}
-					else{
-						ans+= p_noface*(1);
-					}
-				}
+				}				
 			}
 			//for face card
 			if(dealer_sum+10>21){
@@ -58,14 +83,14 @@ void fill_reward(){
 			}
 			else if(dealer_sum+10>=17 && dealer_sum+10<=21){
 				if(dealer_sum+10>player_sum){
-						ans+= p_face*(-1);
-					}
-					else if(dealer_sum+k==player_sum){
-						ans+=0;
-					}
-					else{
-						ans+= p_face*(1);
-					}
+					ans+= p_face*(-1);
+				}
+				else if(dealer_sum+k==player_sum){
+					ans+=0;
+				}
+				else{
+					ans+= p_face*(1);
+				}
 			}
 			else{
 				ans+= p_face*expected_reward[i][dealer_sum+(10-2)];
@@ -82,14 +107,14 @@ void fill_reward(){
 			if(bust){
 				if(dealer_sum+1>=17){
 					if(dealer_sum+1>player_sum){
-							ans+= p_noface*(-1);
-						}
-						else if(dealer_sum+1==player_sum){
-							ans+=0;
-						}
-						else{
-							ans+= p_noface*(1);
-						}
+						ans+= p_noface*(-1);
+					}
+					else if(dealer_sum+1==player_sum){
+						ans+=0;
+					}
+					else{
+						ans+= p_noface*(1);
+					}
 				}
 				else{
 					ans+= p_face*expected_reward[i][dealer_sum+(1-2)];
@@ -98,19 +123,20 @@ void fill_reward(){
 			else{
 				if(dealer_sum+11>=17){
 					if(dealer_sum+11>player_sum){
-							ans+= p_noface*(-1);
-						}
-						else if(dealer_sum+11==player_sum){
-							ans+=0;
-						}
-						else{
-							ans+= p_noface*(1);
-						}
+						ans+= p_noface*(-1);
+					}
+					else if(dealer_sum+11==player_sum){
+						ans+=0;
+					}
+					else{
+						ans+= p_noface*(1);
+					}
 				}
 				else{
 					ans+= p_noface*expected_reward[i][dealer_sum+11-11+14+1];
 				}	
 			}
+			expected_reward[i][j]=ans;
 		}
 		
 	}
