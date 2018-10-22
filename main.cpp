ebc25c09fb656452ea0227b321c7772f6ab5aeba
prajapati-sachin <sachinkp1211@gmail.com>
2018-10-22 23:51:40 +0530

    without ACE reward

--- main.cpp
+++ main.cpp
@@ -16,18 +16,103 @@ float p_noface;
 void fill_reward(){
 	for(int i=0;i<18;i++){
 		int player_sum=i+4;
+		//with ACE
+		for(int j=21;j>=15;j--){
+
+		}
 		//without ACE
 		for(int j=14;j>=0;j--){
 			float ans=0;
 			int dealer_sum=j+2;
+			//for normal card
 			for(int k=2;k<=9;k++){
-
+				if(dealer_sum+k>17){
+					if(dealer_sum+k>player_sum){
+						ans+= p_noface*(-1);
+					}
+					else if(dealer_sum+k==player_sum){
+						ans+=0;
+					}
+					else{
+						ans+= p_noface*(1);
+					}
+				}
+				else if(dealer_sum+k<17){
+					ans+= p_noface*expected_reward[i][j+k];
+				}
+				else{
+					if(dealer_sum+k>player_sum){
+						ans+= p_noface*(-1);
+					}
+					else if(dealer_sum+k==player_sum){
+						ans+=0;
+					}
+					else{
+						ans+= p_noface*(1);
+					}
+				}
+			}
+			//for face card
+			if(dealer_sum+10>21){
+				ans+=p_face*(1);
+			}
+			else if(dealer_sum+10>=17 && dealer_sum+10<=21){
+				if(dealer_sum+10>player_sum){
+						ans+= p_face*(-1);
+					}
+					else if(dealer_sum+k==player_sum){
+						ans+=0;
+					}
+					else{
+						ans+= p_face*(1);
+					}
+			}
+			else{
+				ans+= p_face*expected_reward[i][dealer_sum+(10-2)];
+			}
+			//for ACE
+			bool bust;
+			if(dealer_sum+11>21){
+				bust=true;
+			}
+			else{
+				bust=false;
+			}
+			
+			if(bust){
+				if(dealer_sum+1>=17){
+					if(dealer_sum+1>player_sum){
+							ans+= p_noface*(-1);
+						}
+						else if(dealer_sum+1==player_sum){
+							ans+=0;
+						}
+						else{
+							ans+= p_noface*(1);
+						}
+				}
+				else{
+					ans+= p_face*expected_reward[i][dealer_sum+(1-2)];
+				}
+			}
+			else{
+				if(dealer_sum+11>=17){
+					if(dealer_sum+11>player_sum){
+							ans+= p_noface*(-1);
+						}
+						else if(dealer_sum+11==player_sum){
+							ans+=0;
+						}
+						else{
+							ans+= p_noface*(1);
+						}
+				}
+				else{
+					ans+= p_noface*expected_reward[i][dealer_sum+11-11+14+1];
+				}	
 			}
 		}
-		//with ACE
-		for(int j=15;j<21;j++){
-
-		}
+		
 	}
 }
 
