ebfc64c848d62cea3d9d352928255a8956b2bda1
prajapati-sachin <sachinkp1211@gmail.com>
2018-10-23 22:38:42 +0530

    Values converging

--- main.cpp
+++ main.cpp
@@ -11,7 +11,12 @@ struct state{
 };
 
 double v[10][36];
-double expected_reward[19][21];
+// Hit 1
+// Stand 2
+// Double 3
+// Split 4
+int policy[10][36];
+double expected_reward[21][21];
 double p_face;
 double p_noface;
 
@@ -24,8 +29,9 @@ void initialize_reward(){
 }
 
 void fill_reward(){
-	for(int i=0;i<18;i++){
+	for(int i=0;i<19;i++){
 		int player_sum=i+4;
+		if(i==18) player_sum--;
 		//without ACE 14-9
 		for(int j=14;j>=9;j--){
 			double ans=0;
@@ -40,7 +46,9 @@ void fill_reward(){
 						ans+= p_noface*(-1);
 					}
 					else if(dealer_sum+k==player_sum){
-						ans+=0;
+						if(i!=18) ans+=0;
+						else ans+=p_noface*(1);
+						
 					}
 					else{
 						ans+= p_noface*(1);
@@ -59,7 +67,9 @@ void fill_reward(){
 					ans+= p_face*(-1);
 				}
 				else if(dealer_sum+10==player_sum){
-					ans+=0;
+					if(i!=18) ans+=0;
+					else ans+=p_face*(1);
+						
 				}
 				else{
 					ans+= p_face*(1);
@@ -83,7 +93,9 @@ void fill_reward(){
 						ans+= p_noface*(-1);
 					}
 					else if(dealer_sum+1==player_sum){
-						ans+=0;
+						if(i!=18) ans+=0;
+						else ans+=p_noface*(1);
+						
 					}
 					else{
 						ans+= p_noface*(1);
@@ -99,7 +111,9 @@ void fill_reward(){
 						ans+= p_noface*(-1);
 					}
 					else if(dealer_sum+11==player_sum){
-						ans+=0;
+						if(i!=18) ans+=0;
+						else ans+=p_noface*(1);
+						
 					}
 					else{
 						ans+= p_noface*(1);
@@ -126,7 +140,9 @@ void fill_reward(){
 						ans+= p_noface*(-1);
 					}
 					else if(dealer_sum+k==player_sum){
-						ans+=0;
+						if(i!=18) ans+=0;
+						else ans+=p_noface*(1);
+						
 					}
 					else{
 						ans+= p_noface*(1);
@@ -150,7 +166,9 @@ void fill_reward(){
 						ans+= p_noface*(-1);
 				}
 				else if(dealer_sum+1==player_sum){
-					ans+=0;
+					if(i!=18) ans+=0;
+					else ans+=p_noface*(1);
+						
 				}
 				else{
 					ans+= p_noface*(1);
@@ -175,7 +193,9 @@ void fill_reward(){
 						ans+= p_noface*(-1);
 					}
 					else if(dealer_sum+k==player_sum){
-						ans+=0;
+						if(i!=18) ans+=0;
+						else ans+=p_noface*(1);
+						
 					}
 					else{
 						ans+= p_noface*(1);
@@ -194,7 +214,9 @@ void fill_reward(){
 					ans+= p_face*(-1);
 				}
 				else if(dealer_sum+10==player_sum){
-					ans+=0;
+					if(i!=18) ans+=0;
+					else ans+=p_face*(1);
+						
 				}
 				else{
 					ans+= p_face*(1);
@@ -218,7 +240,9 @@ void fill_reward(){
 						ans+= p_noface*(-1);
 					}
 					else if(dealer_sum+1==player_sum){
-						ans+=0;
+						if(i!=18) ans+=0;
+						else ans+=p_noface*(1);
+						
 					}
 					else{
 						ans+= p_noface*(1);
@@ -234,7 +258,9 @@ void fill_reward(){
 						ans+= p_noface*(-1);
 					}
 					else if(dealer_sum+11==player_sum){
-						ans+=0;
+						if(i!=18) ans+=0;
+						else ans+=p_noface*(1);
+						
 					}
 					else{
 						ans+= p_noface*(1);
@@ -249,27 +275,46 @@ void fill_reward(){
 		
 	}
 	
+	// Second Last row for both blackjack
+	for(int i=0;i<21;i++){
+		expected_reward[19][i]=expected_reward[18][i];
+	}
+	expected_reward[19][8]+=p_noface*(-1);
+
 	// Last row for dealer's blackjack
 	for(int i=0;i<21;i++){
-		expected_reward[18][i]=expected_reward[17][i];
+		expected_reward[20][i]=expected_reward[17][i];
 	}
-	expected_reward[18][8]+=p_noface*(-1);
+	expected_reward[20][8]+=p_noface*(-1);
 }
 
-double find_reward(int playersum, int dealersum, bool dealerface, bool dealerAce){
+double find_reward(int playersum, int dealersum, bool dealerface, bool dealerAce, bool bj){
 	if(playersum!=21){
 		if(dealerAce) return expected_reward[playersum-4][15];
 		else return expected_reward[playersum-4][dealersum-2];
 	}
 	else{
-		if(dealerface){
-			return expected_reward[18][dealersum-2];
-		}
-		else if(dealerAce){
-			return expected_reward[playersum-4][15];
+		if(bj){
+			if(dealerface){
+				return expected_reward[19][dealersum-2];
+			}
+			else if(dealerAce){
+				return expected_reward[19][15];
+			}
+			else{
+				return expected_reward[19][dealersum-2];
+			}
 		}
 		else{
-			return expected_reward[playersum-4][dealersum-2];
+			if(dealerface){
+				return expected_reward[20][dealersum-2];
+			}
+			else if(dealerAce){
+				return expected_reward[playersum-4][15];
+			}
+			else{
+				return expected_reward[playersum-4][dealersum-2];
+			}
 		}
 	}
 }
@@ -288,7 +333,7 @@ void initialize_v(){
 	}
 }
 
-double values(int cat, int sum, int dcard){
+double value(int cat, int sum, int dcard){
 	int index1;
 	int index2;
 	if(cat==1){
@@ -298,25 +343,29 @@ double values(int cat, int sum, int dcard){
 	}
 	else if(cat==2){
 		index1= dcard-2;
-		index2= psum-2 + 17;
+		index2= sum-2 + 17;
 		return v[index1][index2];
 	}
 	else{
 		index1= dcard-2;
-		index2= psum-2 + 25;
+		index2= sum-2 + 25;
 		return v[index1][index2];	
 	}
 }
 
-void value_iteration(){
+double value_iteration(){
+	double max_change=0;
 	for(int i=0;i<10;i++){
+		double Vn[36];
+		int polic[36];
 		// 11 for ACE
 		int dealercard=i+2;
-		for(int j=0;j<35;j++){
+		for(int j=0;j<36;j++){
 			int category=0;
 			if(j>=0 && j<=16)category=1;
-			else if(j>=17 && j<=24)category=2;
-			else category=3;
+			else if(j>=17 && j<=25)category=2;
+			else if (j>=26 && j<=34)category=3;
+			else category=4;
 			// Normal Sum
 			if(category==1){
 				int psum=j+5;
@@ -328,7 +377,7 @@ void value_iteration(){
 				if(i==8) isFace=true;
 				if(i==9) isAce=true;
 				// STAND REWARD
-					stand_reward=find_reward(psum, dealercard, isFace, isAce);
+					stand_reward=find_reward(psum, dealercard, isFace, isAce, false);
 				// HIT REWARD
 					//ace
 					if(psum+11>21){
@@ -352,7 +401,7 @@ void value_iteration(){
 						hit_reward+= p_face*(-1);						
 					}
 					else{
-						hit_reward+= p_face*value(1, psum+k, dealercard);
+						hit_reward+= p_face*value(1, psum+10, dealercard);
 					}
 
 				// DOUBLE REWARD
@@ -360,11 +409,11 @@ void value_iteration(){
 					if(psum+11>21){
 						if(psum+1>21) double_reward+= p_noface*(-1);
 						else{
-							double_reward+= p_noface*2*(find_reward(psum+1, dealercard, isFace, isAce));
+							double_reward+= p_noface*2*(find_reward(psum+1, dealercard, isFace, isAce, false));
 						}
 					}
 					else{
-						double_reward+= p_noface*2*(find_reward(psum+11, dealercard, isFace, isAce));						
+						double_reward+= p_noface*2*(find_reward(psum+11, dealercard, isFace, isAce, false));						
 					}
 					//normal card
 					for(int k=2;k<=9;k++){
@@ -372,7 +421,7 @@ void value_iteration(){
 							double_reward+= p_noface*(-1);
 						}
 						else{
-							double_reward+= p_noface*2*find_reward(psum+k, dealercard, isFace, isAce);
+							double_reward+= p_noface*2*find_reward(psum+k, dealercard, isFace, isAce, false);
 						}	
 					}
 					//face card
@@ -380,59 +429,356 @@ void value_iteration(){
 						double_reward+= p_face*(-1);						
 					}
 					else{
-						double_reward+= p_face*2*find_reward(psum+10, dealercard, isFace, isAce);
+						double_reward+= p_face*2*find_reward(psum+10, dealercard, isFace, isAce, false);
 					}
+
+				//update V value
+				Vn[j]= max(stand_reward, max(double_reward, hit_reward));
+				if(Vn[j]==hit_reward){
+					polic[j]=1;
+				}
+				else if(Vn[j]==double_reward){
+					polic[j]=3;
+				}
+				else{
+					polic[j]=2;
+				}
+
 			}
 			// One Ace
 			else if(category==2){
+				int psum=(j-15)+11;
+				double double_reward=0;
+				double stand_reward=0;
+				double hit_reward=0;
+				bool isAce=false;
+				bool isFace=false;
+				if(i==8) isFace=true;
+				if(i==9) isAce=true;
+				// STAND REWARD
+					stand_reward=find_reward(psum, dealercard, isFace, isAce, false);
+				// HIT REWARD
+					//ace
+					if(psum+1>21){
+						hit_reward+= p_noface*value(1, psum-10+1, dealercard);
+					}
+					else{
+						hit_reward+= p_noface*value(2, psum-11+1, dealercard);
+					}
+					//normal card
+					for(int k=2;k<=9;k++){
+						if(psum+k>21){
+							hit_reward+= p_noface*value(1, psum-10+k, dealercard);
+						}
+						else{
+							hit_reward+= p_noface*value(2, psum-11+k, dealercard);
+						}	
+					}
+					//face card
+					if(psum+10>21){
+						hit_reward+= p_face*value(1, psum-10+10, dealercard);						
+					}
+					else{
+						hit_reward+= p_face*value(1, psum+10, dealercard);
+					}
 
+				// DOUBLE REWARD
+					//ace
+					if(psum+1>21){
+						double_reward+= p_noface*2*(find_reward(psum-10+1, dealercard, isFace, isAce, false));
+					}
+					else{
+						double_reward+= p_noface*2*(find_reward(psum+1, dealercard, isFace, isAce, false));						
+					}
+					//normal card
+					for(int k=2;k<=9;k++){
+						if(psum+k>21){
+							double_reward+= p_noface*2*(find_reward(psum-10+k, dealercard, isFace, isAce, false));
+						}
+						else{
+							double_reward+= p_noface*2*find_reward(psum+k, dealercard, isFace, isAce, false);
+						}	
+					}
+					//face card
+					if(psum+10>21){
+						double_reward+= p_face*2*find_reward(psum, dealercard, isFace, isAce, false);						
+					}
+					else{
+						double_reward+= p_face*2*find_reward(psum+10, dealercard, isFace, isAce, false);
+					}
+
+				//update V value
+				Vn[j]= max(stand_reward, max(double_reward, hit_reward));
+				if(Vn[j]==hit_reward){
+					polic[j]=1;
+				}
+				else if(Vn[j]==double_reward){
+					polic[j]=3;
+				}
+				else{
+					polic[j]=2;
+				}
 			}
 			//Pair
-			else{
+			else if(category==3){
+				int psum=2*(j-24);
+				double double_reward=0;
+				double stand_reward=0;
+				double hit_reward=0;
+				double split_reward=0;
+				bool isAce=false;
+				bool isFace=false;
+				if(i==8) isFace=true;
+				if(i==9) isAce=true;
+				// STAND REWARD
+					stand_reward=find_reward(psum, dealercard, isFace, isAce, false);
+				// HIT REWARD
+					//ace
+					if(psum+11>21){
+						if(psum+1>21) hit_reward+= p_noface*(-1);
+						else hit_reward+= p_noface*value(1, psum+1, dealercard);
+					}
+					else{
+						hit_reward+= p_noface*value(2, psum, dealercard);
+					}
+					//normal card
+					for(int k=2;k<=9;k++){
+						if(psum+k>21){
+							hit_reward+= p_noface*(-1);
+						}
+						else{
+							hit_reward+= p_noface*value(1, psum+k, dealercard);
+						}	
+					}
+					//face card
+					if(psum+10>21){
+						hit_reward+= p_face*(-1);						
+					}
+					else{
+						hit_reward+= p_face*value(1, psum+10, dealercard);
+					}
 
+				// DOUBLE REWARD
+					//ace
+					if(psum+11>21){
+						if(psum+1>21) double_reward+= p_noface*(-1);
+						else{
+							double_reward+= p_noface*2*(find_reward(psum+1, dealercard, isFace, isAce, false));
+						}
+					}
+					else{
+						double_reward+= p_noface*2*(find_reward(psum+11, dealercard, isFace, isAce, false));						
+					}
+					//normal card
+					for(int k=2;k<=9;k++){
+						if(psum+k>21){
+							double_reward+= p_noface*(-1);
+						}
+						else{
+							double_reward+= p_noface*2*find_reward(psum+k, dealercard, isFace, isAce, false);
+						}	
+					}
+					//face card
+					if(psum+10>21){
+						double_reward+= p_face*(-1);						
+					}
+					else{
+						double_reward+= p_face*2*find_reward(psum+10, dealercard, isFace, isAce, false);
+					}
+
+				//SPLIT REWARD
+					//ace
+					int card=psum/2;
+					if(card!=10){
+						split_reward+=p_noface*value(2, card, dealercard);
+					}
+					else{
+						//player blackjack
+						split_reward+=p_noface*find_reward(psum+1, dealercard, isFace, isAce, true);						
+					}
+					//normal cards
+					for(int k=2;k<=9;k++){
+						split_reward+=p_noface*value(1, card+k, dealercard);	
+					}
+					//face
+					split_reward+=p_face*value(1, card+10, dealercard);
+					split_reward*=2;
+				//update V value
+				Vn[j]= max(split_reward, max(stand_reward, max(double_reward, hit_reward)));
+				if(Vn[j]==hit_reward){
+					polic[j]=1;
+				}
+				else if(Vn[j]==double_reward){
+					polic[j]=3;
+				}
+				else if(Vn[j]==split_reward){
+					polic[j]=4;
+				}
+				else{
+					polic[j]=2;
+				}
+			}
+			//Pair of ACE
+			else{
+				int psum=12;
+				double double_reward=0;
+				double stand_reward=0;
+				double hit_reward=0;
+				double split_reward=0;
+				bool isAce=false;
+				bool isFace=false;
+				if(i==8) isFace=true;
+				if(i==9) isAce=true;
+				// STAND REWARD
+					stand_reward=find_reward(psum, dealercard, isFace, isAce, false);
+				// HIT REWARD
+					//ace
+					if(psum+11>21){
+						if(psum+1>21) hit_reward+= p_noface*(-1);
+						else hit_reward+= p_noface*value(1, psum+1, dealercard);
+					}
+					else{
+						hit_reward+= p_noface*value(2, psum, dealercard);
+					}
+					//normal card
+					for(int k=2;k<=9;k++){
+						if(psum+k>21){
+							hit_reward+= p_noface*(-1);
+						}
+						else{
+							hit_reward+= p_noface*value(1, psum+k, dealercard);
+						}	
+					}
+					//face card
+					if(psum+10>21){
+						hit_reward+= p_face*value(1, psum-10+10, dealercard);						
+					}
+					else{
+						hit_reward+= p_face*value(1, psum+10, dealercard);
+					}
+
+
+				// DOUBLE REWARD
+					//ace
+					if(psum+11>21){
+						if(psum+1>21) double_reward+= p_noface*(-1);
+						else{
+							double_reward+= p_noface*2*(find_reward(psum+1, dealercard, isFace, isAce, false));
+						}
+					}
+					else{
+						double_reward+= p_noface*2*(find_reward(psum+11, dealercard, isFace, isAce, false));						
+					}
+					//normal card
+					for(int k=2;k<=9;k++){
+						if(psum+k>21){
+							double_reward+= p_noface*(-1);
+						}
+						else{
+							double_reward+= p_noface*2*find_reward(psum+k, dealercard, isFace, isAce, false);
+						}	
+					}
+					//face card
+					if(psum+10>21){
+						double_reward+= p_face*2*find_reward(psum, dealercard, isFace, isAce, false);						
+					}
+					else{
+						double_reward+= p_face*2*find_reward(psum+10, dealercard, isFace, isAce, false);
+					}
+
+				//SPLIT REWARD
+					//ace
+						split_reward+=p_noface*find_reward(psum, dealercard, isFace, isAce, false);						
+					//normal cards
+					for(int k=2;k<=9;k++){
+						split_reward+=p_noface*find_reward(psum-1+k, dealercard, isFace, isAce, false);	
+					}
+					//face
+					split_reward+=p_face*find_reward(psum-1+10, dealercard, isFace, isAce, false);
+					split_reward*=2;
+				//update V value
+				Vn[j]= max(split_reward, max(stand_reward, max(double_reward, hit_reward)));
+				if(Vn[j]==hit_reward){
+					polic[j]=1;
+				}
+				else if(Vn[j]==double_reward){
+					polic[j]=3;
+				}
+				else if(Vn[j]==split_reward){
+					polic[j]=4;
+				}
+				else{
+					polic[j]=2;
+				}
 			}
 
 		}
+	//update v
+		for(int m=0;m<36;m++){
+			double update= abs(Vn[m]-v[i][m]);
+			if(update>max_change){
+				max_change=update;
+			}
+			v[i][m]=Vn[m];
+			policy[i][m]=polic[m];	
+		}
 	}
+	return max_change;
 }
 
-// void find_policy(){
 
-// }
+void write_policy(){
+	
 
-// void write_policy(){
 
-// }
+
+}
 
 int main(int argc, char const *argv[]){
 	srand(time(0));
 	p_face= atof(argv[1]);
 	p_noface= (1-p_face)/9.0;
 	
-	// initialize_reward();
+	initialize_reward();
 	fill_reward();
-	for(int i=0;i<19;i++){
-		cout << i+4 << ": ";
-		for(int j=0;j<9;j++){
-			cout << expected_reward[i][j] << " ";
-		}
-		cout << expected_reward[i][15];
-		cout << endl;
-	}
+	// for(int i=0;i<21;i++){
+	// 	cout << i+4 << ": ";
+	// 	for(int j=0;j<9;j++){
+	// 		cout << expected_reward[i][j] << " ";
+	// 	}
+	// 	cout << expected_reward[i][15];
+	// 	cout << endl;
+	// }
 
-		cout << endl;
+	// 	cout << endl;
 
-		cout << endl;
+	// 	cout << endl;
 
-	for(int i=0;i<19;i++){
-		cout << i+4 << ": ";
-		for(int j=0;j<21;j++){
-			cout << expected_reward[i][j] << " ";
-		}
-		// cout << expected_reward[i][15];
-		cout << endl;
-	}
+	// for(int i=0;i<21;i++){
+	// 	cout << i+4 << ": ";
+	// 	for(int j=0;j<21;j++){
+	// 		cout << expected_reward[i][j] << " ";
+	// 	}
+	// 	// cout << expected_reward[i][15];
+	// 	cout << endl;
+	// }
 	initialize_v();
+	for(int i=0;i<10;i++){
+		cout << i << ": ";
+		for(int j=0;j<33;j++){
+			cout << v[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << endl;
+	cout << endl;
+
+	//repeat
+	// for(int i=0;i<20;i++){
+	// 	cout <<"Max change iteration" <<value_iteration() << endl;
+	// }
+	
 	// for(int i=0;i<10;i++){
 	// 	cout << i << ": ";
 	// 	for(int j=0;j<33;j++){
@@ -440,9 +786,8 @@ int main(int argc, char const *argv[]){
 	// 	}
 	// 	cout << endl;
 	// }
-	//repeat
-	// value_iteration();
+
 	// find_policy();
-	// write_policy();
+	write_policy();
 
 }
